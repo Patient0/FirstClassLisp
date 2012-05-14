2b3c8131aa7dd9256c264f26cd2f911d5f2f9f36
Paul Hollingsworth <paul@paulhollingsworth.com>
2012-05-14 23:34:03 +0100

    Add evaluator tests for atom and symbol

--- Lisp/LispEngine/Evaluation/Evaluator.cs
+++ Lisp/LispEngine/Evaluation/Evaluator.cs
@@ -8,8 +8,16 @@ namespace LispEngine.Evaluation
 {
     public sealed class Evaluator
     {
-        public Datum evaluate(Environment env, Datum datum)
+        public object evaluate(Environment env, Datum datum)
         {
+            // Atoms evaluate to themselves
+            var a = datum as Atom;
+            if (a != null)
+                return a.Value;
+            var s = datum as Symbol;
+            if(s != null)
+                return env.lookup(s.Identifier);
+            
             return null;
         }
     }
--- Lisp/LispTests/Evaluation/EvaluatorTest.cs
+++ Lisp/LispTests/Evaluation/EvaluatorTest.cs
@@ -3,21 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using LispEngine.Datums;
+using LispEngine.Evaluation;
+using LispEngine.Lexing;
+using LispEngine.Parsing;
 using NUnit.Framework;
+using Environment = LispEngine.Evaluation.Environment;
 
 namespace LispTests.Evaluation
 {
     [TestFixture]
-    class EvaluatorTest
+    class EvaluatorTest : DatumHelpers
     {
-        private static void test(string sexp, Datum expected)
+        private class MockEnvironment : Environment
         {
-            
+            private readonly IDictionary<string, object> items = new Dictionary<string, object>();
+            public object lookup(string name)
+            {
+                if(items.ContainsKey(name))
+                    return items[name];
+                return EmptyEnvironment.Instance.lookup(name);
+            }
+
+            public void Put(String name, object value)
+            {
+                items[name] = value;
+            }
+        }
+
+        private readonly MockEnvironment env = new MockEnvironment();
+
+        public EvaluatorTest()
+        {
+            env.Put("life", 42);
+        }
+
+        private void test(string sexp, object expected)
+        {
+            var e = new Evaluator();
+
+            var datum = new Parser(Scanner.Create(sexp).Scan()).parse();
+            var result = e.evaluate(env, datum);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
         public void testAtom()
         {
+            test("23", 23);
+        }
+
+        [Test]
+        public void testSymbol()
+        {
+            test("life", 42);
         }
     }
 }
