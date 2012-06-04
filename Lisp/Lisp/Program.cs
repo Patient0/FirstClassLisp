0893abcd5cbe14be064f4111f97d01087382b73e
Paul Hollingsworth <paul@paulhollingsworth.com>
2012-06-04 11:16:01 +0100

    Add a REPL to Lisp.exe
    Add a really simple Read Evaluate Print Loop
    to wrap around the engine.

--- Lisp/Lisp/Program.cs
+++ Lisp/Lisp/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using LispEngine.Bootstrap;
+using LispEngine.Evaluation;
+using LispEngine.Lexing;
+using LispEngine.Parsing;
 
 namespace Lisp
 {
@@ -9,6 +13,19 @@ namespace Lisp
     {
         static void Main(string[] args)
         {
+            // Really simple REPL
+            var env = StandardEnvironment.Create();
+            string line;
+            var evaluator = new Evaluator();
+            Console.WriteLine("First Class Lisp. Ctrl-Z + Enter to exit.");
+            Console.Write("FCLisp> ");
+            while((line = Console.ReadLine()) != null)
+            {
+                var parsed = new Parser(Scanner.Create(line)).parse();
+                var result = evaluator.Evaluate(env, parsed);
+                Console.WriteLine("{0}", result);
+                Console.Write("FCLisp> ");
+            }
         }
     }
 }
