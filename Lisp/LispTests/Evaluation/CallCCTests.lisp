73447175db50e24ebe2edf30b1d9ea3207e12b08
Paul Hollingsworth <paul@paulhollingsworth.com>
2012-06-08 23:53:09 +0100

    Add some more call/cc tests

--- Lisp/LispTests/Evaluation/CallCCTests.lisp
+++ Lisp/LispTests/Evaluation/CallCCTests.lisp
@@ -2,3 +2,7 @@
 (simplestCallCC 5
     (call/cc (lambda (c)
             (+ 3 (c 5)))))
+(noopCallCC 5
+    ((call/cc call/cc) (lambda (x) 5)))
+(backwardsCC 5
+    ((call/cc (lambda (k) k)) (lambda (x) 5)))
