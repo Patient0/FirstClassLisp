36fea900e9803843f89e1a410e09049807c25fa2
Paul Hollingsworth <paul@paulhollingsworth.com>
2012-07-16 22:13:30 +0100

    Add 'units' assoc-list for sudoku solver

--- Lisp/LispTests/Evaluation/SudokuTests.lisp
+++ Lisp/LispTests/Evaluation/SudokuTests.lisp
@@ -12,10 +12,15 @@
                 '((1 2 3) (4 5 6) (7 8 9)))
             (loop c cols (cross rows (list c)))
             (loop r rows (cross (list r) cols))))
+
+    (define (units-for-square s)
+        (filter (lambda (unit)
+                    (in s unit))
+                unitlist))
+
     (define units
         (make-dict (loop s squares
-                   (cons s
-                       (filter (lambda (u) (in s u)) unitlist)))))
+                       (cons s (units-for-square s)))))
                             
 )
 (tests
@@ -26,4 +31,10 @@
     (unitlist
         27
         (length unitlist))
+
+    (units
+        (((A . 1) (A . 2) (A . 3) (B . 1) (B . 2) (B . 3) (C . 1) (C . 2) (C . 3))
+         ((A . 2) (B . 2) (C . 2) (D . 2) (E . 2) (F . 2) (G . 2) (H . 2) (I . 2))
+         ((C . 1) (C . 2) (C . 3) (C . 4) (C . 5) (C . 6) (C . 7) (C . 8) (C . 9)))
+        (lookup units '(C . 2)))
 )
